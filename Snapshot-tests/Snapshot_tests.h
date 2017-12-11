61df897e4839c2d81f5f599e0be3721d00d46011
AndriiDoroshko <Andr.dorosh4@gmail.com>
2017-12-11 17:42:45 +0200

    Library for snapshot testing

---
+++ Snapshot-tests/Snapshot_tests.h
@@ -0,0 +1,19 @@
+//
+//  Snapshot_tests.h
+//  Snapshot-tests
+//
+//  Created by Andrey Doroshko on 12/11/17.
+//  Copyright © 2017 Andrey Doroshko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Snapshot_tests.
+FOUNDATION_EXPORT double Snapshot_testsVersionNumber;
+
+//! Project version string for Snapshot_tests.
+FOUNDATION_EXPORT const unsigned char Snapshot_testsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Snapshot_tests/PublicHeader.h>
+
+
